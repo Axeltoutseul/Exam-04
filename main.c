f584f596515a80c4940cd3c8e0ecbdee0c365810
Axel Baudrillart <axbaudri@f2r8s13.clusters.42paris.fr>
2025-08-08 18:11:57 +0200

    microshell

--- main.c
+++ main.c
@@ -1,56 +1,60 @@
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
 
-int send_error(const char *str)
+void send_error(const char *str)
 {
-    int i = 0;
-    while (str[i])
-        write(2, &str[i++], 1);
-    return (1);
+    while (*str)
+        write(2, &*str++, 1);
 }
 
-int cd(int i, char **argv)
+void cd(int i, char **argv)
 {
     if (i != 2)
-        return send_error("Bad argument\n");
+        return send_error("error: cd: bad arguments\n");
     if (chdir(argv[1]) == -1)
-        return send_error("chdir\n");
-    return 0;
+        return send_error("error: cd: cannot change directory to "), send_error(argv[1]), send_error("\n");
 }
 
-int micro_shell(char **argv, int i, char **envp)
+void microshell(int i, char **argv, char **envp)
 {
-    int pipe_fd[2];
+    pid_t pid;
+    int fds[2];
     int status = 0;
     int has_pipe = argv[i] && strcmp(argv[i], "|") == 0;
-
     if (!has_pipe && strcmp(argv[0], "cd") == 0)
         return cd(i, argv);
-    if (has_pipe && pipe(pipe_fd) == -1)
-        return (send_error("pipe"));
-    int pid = fork();
+    if (has_pipe && pipe(fds) == -1)
+    {
+        send_error("error: fatal\n");
+        exit(EXIT_FAILURE);
+    }
+    pid = fork();
     if (pid == 0)
     {
-        argv[i] = 0;
-        if (has_pipe && (dup2(pipe_fd[1], 1) == -1 || close(pipe_fd[0]) == -1 || close(pipe_fd[1]) == -1))
-            return (send_error("dup2"));
+        argv[i] = NULL;
         if (strcmp(argv[0], "cd") == 0)
             return cd(i, argv);
+        if (has_pipe && (dup2(fds[1], 1) == -1 || close(fds[0]) == -1 || close(fds[1]) == -1))
+        {
+            send_error("error: fatal\n");
+        	exit(EXIT_FAILURE);
+        }
         execve(*argv, argv, envp);
-        return (send_error("execve\n"));
+        send_error("error: cannot execute "), send_error(argv[0]), send_error("\n");
     }
     waitpid(pid, &status, 0);
-    if (has_pipe && (dup2(pipe_fd[0], 0) == -1 || close(pipe_fd[0]) == -1 || close(pipe_fd[1]) == -1))
-        send_error("dup2");
-    return 0;
+    if (has_pipe && (dup2(fds[0], 0) == -1 || close(fds[0]) == -1 || close(fds[1]) == -1))
+    {
+        return send_error("error: fatal\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 int main(int argc, char **argv, char **envp)
 {
     int i = 0;
-    int status = 0;
-
     if (argc < 2)
         return -1;
     while (argv[i] && i < argc - 1)
@@ -58,10 +62,10 @@ int main(int argc, char **argv, char **envp)
         ++i;
         argv += i;
         i = 0;
-        while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+        while (argv[i] && strcmp(argv[i], "|") != 0 && strcmp(argv[i], ";") != 0)
             i++;
         if (i > 0)
-            status = micro_shell(argv, i, envp);
+            microshell(i, argv, envp);
     }
     return 0;
 }
